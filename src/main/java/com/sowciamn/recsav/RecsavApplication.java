d08e197e325b2a076e741c6a1aa101cdbbd3bf0a
kiyan <sowciamn@gmail.com>
2025-07-01 23:05:37 +0900

    categoryのAPIを作成

---
+++ src/main/java/com/sowciamn/recsav/RecsavApplication.java
@@ -0,0 +1,13 @@
+package com.sowciamn.recsav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecsavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecsavApplication.class, args);
+	}
+
+}
